5ef810045fa0219fafe8dfa162e425dfed93111a
Dmitry <mail@gornak.me>
2018-08-31 15:35:30 +0300

    fixes

--- screens/ReaderScreenDetail.js
+++ screens/ReaderScreenDetail.js
@@ -34,37 +34,6 @@ class EpubReader extends Component {
         };
         this.streamer = new Streamer();
         console.log('constructor props: ', this.props)
-        if (!Object.assign) {
-            Object.defineProperty(Object, 'assign', {
-              enumerable: false,
-              configurable: true,
-              writable: true,
-              value: function(target, firstSource) {
-                'use strict';
-                if (target === undefined || target === null) {
-                  throw new TypeError('Cannot convert first argument to object');
-                }
-          
-                var to = Object(target);
-                for (var i = 1; i < arguments.length; i++) {
-                  var nextSource = arguments[i];
-                  if (nextSource === undefined || nextSource === null) {
-                    continue;
-                  }
-          
-                  var keysArray = Object.keys(Object(nextSource));
-                  for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
-                    var nextKey = keysArray[nextIndex];
-                    var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
-                    if (desc !== undefined && desc.enumerable) {
-                      to[nextKey] = nextSource[nextKey];
-                    }
-                  }
-                }
-                return to;
-              }
-            });
-          }
     }
     static navigationOptions = ({navigation}) => {
         const bookName = navigation.getParam('book_name')
@@ -218,7 +187,8 @@ const styles = StyleSheet.create({
     reader: {
         flex: 1,
         alignSelf: 'stretch',
-        backgroundColor: '#3F3F3C'
+        backgroundColor: '#3F3F3C',
+        paddingBottom: 100
     },
     bar: {
         position:"absolute",

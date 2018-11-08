02130fd530f4114132898f161e0306533f78172e
Dmitry <mail@gornak.me>
2018-11-09 00:00:25 +0500

    cache covers

--- screens/ReaderScreen.js
+++ screens/ReaderScreen.js
@@ -14,6 +14,7 @@ import {
 import { API_URL } from '../constants/api';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { listStyles } from '../constants/list_styles';
+import RNFetchBlob from 'rn-fetch-blob'
 export default class ReaderScreen extends Component {
   constructor(props){
     super(props);
@@ -23,7 +24,9 @@ export default class ReaderScreen extends Component {
       refreshnig: false,
       pages_count: 0,
       current_page: 1,
-      online: true
+      online: true,
+      covers_fired: false,
+      downloaded_covers: [],
     }
   }
   static navigationOptions = {
@@ -60,6 +63,12 @@ export default class ReaderScreen extends Component {
             }
             })
             AsyncStorage.setItem('cache_reader_list', request.responseText);
+            if (!this.state.covers_fired){
+              this.downloadCovers();
+              this.setState({
+                covers_fired: true
+              })
+            }
         } else {
           console.log('error reader books req');
           AsyncStorage.getItem('cache_reader_list', (err, value) => {
@@ -84,7 +93,7 @@ export default class ReaderScreen extends Component {
   setPagination(){
     try {
       let books_count = this.state.books.length;
-      console.log('setpagination', books_count)
+      // console.log('setpagination', books_count)
       this.setState({
         pages_count: Math.ceil(books_count/5)
       });
@@ -101,6 +110,72 @@ export default class ReaderScreen extends Component {
     console.log("CDM")
     // AsyncStorage.clear();
   }
+  downloadCovers(){
+    console.log('downloadCovers() fired')
+    AsyncStorage.getItem('downloaded_covers', (err, value) => {
+      console.log('downloaded_covers value', value)
+      if (value){
+        this.downloaded_covers = JSON.parse(value);
+        this.setState({
+          downloaded_covers: this.downloaded_covers
+        })
+        if (this.state.books.length != this.downloaded_covers.length){
+          this.need_to_download_covers = []
+          this.state.books.forEach(book => {
+            let isDownloaded = false;
+            this.downloaded_covers.forEach(cover => {
+              if (book.id == cover.id){
+                isDownloaded = true;
+              }
+            });
+            if (!isDownloaded){
+              this.need_to_download_covers.push(book);
+            }
+          });
+          this.downloadCoverQueue();  
+        }
+      } else {
+        this.downloaded_covers = [];
+        this.need_to_download_covers = [].concat(this.state.books);
+        this.downloadCoverQueue();
+      }
+    });
+  }
+  downloadCoverQueue(){
+    if (this.need_to_download_covers.length){
+      console.log('this.need_to_download_covers', this.need_to_download_covers)
+      let book_to_download = this.need_to_download_covers[0];
+      console.log('book_to_download', book_to_download);
+      this.task = RNFetchBlob
+        .config({
+          fileCache: true,
+          appendExt : 'jpg',
+        })
+        .fetch('GET', 'https://mobile-app.flamesclient.ru/'+book_to_download.cover_src, {});
+      this.task
+        .progress((received, total) => {
+          console.log('progress', received / total)
+        })
+      this.task
+        .then((res) => {
+            console.log('The file saved to ', res.path())
+            this.downloaded_covers.push({
+              id: book_to_download.id,
+              file_path: res.path(),
+            })
+            AsyncStorage.setItem('downloaded_covers', JSON.stringify(this.downloaded_covers))
+            this.setState({
+              downloaded_covers: this.downloaded_covers,
+            })
+            this.need_to_download_covers.shift();
+            this.downloadCoverQueue();
+        });
+    } else {
+      this.setState({
+        downloaded_covers: this.downloaded_covers,
+      })
+    }
+  }
   render() {
     // console.log('render', this.state)
     let comp;
@@ -113,41 +188,67 @@ export default class ReaderScreen extends Component {
         pagination_arr.push(i);
       }
     }
-    console.log(pagination_arr);
+    // console.log(pagination_arr);
+    console.log('render state', this.state);
     if (true) {
       comp = (
         <SafeAreaView style={{flex: 1, backgroundColor: '#efefef'}}>
             <FlatList
               style={{paddingLeft: 10, paddingRight: 10, paddingBottom: 5, paddingTop: 5}}
               data={books_on_page}
-              renderItem={({item}) => (
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('Reader', {book_id: item.id, book_name: item.name, book_src: item.file_src} )}>
-                  <View style={listStyles.quoteItem}>
-                    <View>
-                      <View style={listStyles.bookTop}>
-                      {this.state.online && (
-                        <View style={{marginRight: 10}}>
-                          {item.cover_src && (
-                            <Image
-                            source={{uri: 'https://mobile-app.flamesclient.ru/' + item.cover_src}}
-                            style={{width: 80, height: 120}}
-                            />
-                          )}
-                        </View>
-                      )}
-                        <View style={{flexWrap: 'wrap', flex: 1}}>
-                          <Text style={listStyles.quoteTitle}>{item.name}</Text>
-                          <Text style={{marginTop: 10, color: '#c5c5c5', fontStyle: 'italic'}}>{item.author}</Text>
-                        </View>
+              renderItem={({item}) => {
+                // console.log('render item )')
+                let cover_src = null;
+                let view;
+                this.state.downloaded_covers.forEach(cover => {
+                  if (item.id == cover.id){
+                    cover_src = cover.file_path;
+                    // console.log('render item 1')
+                    view = (
+                      <View style={{marginRight: 10}}>
+                          <Image
+                          source={{uri: cover_src}}
+                          style={{width: 80, height: 120}}
+                          />
                       </View>
-                      <Text style={{marginTop: 10}}>{item.description}</Text>
+                    )
+                  }
+                });
+                if ((this.state.online) && (!cover_src)){
+                  cover_src = 'https://mobile-app.flamesclient.ru/' + item.cover_src;
+                  view = (
+                    <View style={{marginRight: 10}}>
+                        <Image
+                        source={{uri: cover_src}}
+                        style={{width: 80, height: 120}}
+                        />
                     </View>
-                    {/* <View>
-                      <Ionicons name="ios-arrow-forward" size={25} color="tomato"/>
-                    </View> */}
-                  </View>
-                </TouchableOpacity>
-              )}
+                  )
+                }
+                if ((!this.state.online) && (!cover_src)){
+                  view = null;
+                  // console.log('render item 2')
+                }
+                return (
+                  <TouchableOpacity onPress={() => this.props.navigation.navigate('Reader', {book_id: item.id, book_name: item.name, book_src: item.file_src} )}>
+                    <View style={listStyles.quoteItem}>
+                      <View>
+                        <View style={listStyles.bookTop}>
+                          {view}
+                          <View style={{flexWrap: 'wrap', flex: 1}}>
+                            <Text style={listStyles.quoteTitle}>{item.name}</Text>
+                            <Text style={{marginTop: 10, color: '#c5c5c5', fontStyle: 'italic'}}>{item.author}</Text>
+                          </View>
+                        </View>
+                        <Text style={{marginTop: 10}}>{item.description}</Text>
+                      </View>
+                      {/* <View>
+                        <Ionicons name="ios-arrow-forward" size={25} color="tomato"/>
+                      </View> */}
+                    </View>
+                  </TouchableOpacity>
+                )
+              }}
               keyExtractor={this._keyExtractor}
               onRefresh={() => this.getBooks()}
               refreshing={false}
